08fbc46f573be03e0f01237ad63b6aaf20daa23c
Azamat Kurbanaev <azkurban@gmail.com>
2020-05-11 01:12:46 +0300

    ServiceMan: Native API added

---
+++ ServiceMan/Model/ServiceProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ServiceMan.Model
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    internal struct ServiceProcess
+    {
+        public int ProcessId;
+        public string Name;
+        public string Description;
+        public string ImagePath;
+        public string Status;
+    };
+}
---
+++ ServiceMan/Services/NativeAPI.cs
@@ -0,0 +1,21 @@
+﻿using ServiceMan.Model;
+using System.Runtime.InteropServices;
+
+namespace ServiceMan.Services
+{
+    /// <summary>
+    /// http://msdn.microsoft.com/en-us/library/aa288468(VS.71).aspx
+    /// http://www.mono-project.com/docs/advanced/pinvoke/
+    /// </summary>
+    internal static class NativeAPI
+    {
+        private const string DLL_LOCATION = @"x64\svcman.dll";
+
+        [DllImport(DLL_LOCATION, CallingConvention = CallingConvention.Cdecl)]
+        public static extern ulong ServiceCount();
+       
+        [DllImport(DLL_LOCATION, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ServiceList([In, Out] ServiceProcess[] services, ulong count);
+
+    }
+}
